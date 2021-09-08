9c7f7ae263ac562b6a57b929c9424744eed73324
loxygenk <me@loxygen.dev>
2021-09-08 22:09:59 +0900

    🔧 Configurate webpack-dev-server with the maximum effort

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,14 @@
 import path from "path"
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
-import webpack from "webpack";
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
-const configuration: webpack.Configuration = {
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration
+}
+
+const configuration: Configuration = {
   // TODO: Change this using environemnt variable
   mode: "development",
   entry: path.resolve(__dirname, "src/index.tsx"),
@@ -27,7 +32,12 @@ const configuration: webpack.Configuration = {
       template: path.resolve(__dirname, "src", "template.html"),
       scriptLoading: "defer"
     })
-  ]
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
 }
 
 export default configuration;
