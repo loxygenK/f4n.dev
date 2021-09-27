ddf4e14d97fac97699f924f9bf12614de80199f0
loxygenk <me@loxygen.dev>
2021-09-27 11:26:01 +0900

    💄 Place things center and set background

---
+++ src/comps/layout/CotentWrapper.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./ContentWrapper.module.scss";
+
+export type ContentWrapperProps = {
+  children: React.ReactNode
+}
+export const ContentWrapper = (props: ContentWrapperProps) => (
+  <div className={styles.contentWrapper}>
+    {props.children}
+  </div>
+)
