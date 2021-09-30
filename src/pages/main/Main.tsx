997db23f1103f4f25780898808acf7ee79acdbdb
loxygenk <me@loxygen.dev>
2021-10-01 01:01:14 +0900

    ⭐ Adjust the logo animation with the best effort

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { PageWrapper } from "~/comps/layout/PageWrapper";
 import {LogoImage} from "~/comps/shared/LogoImage";
-import {LogoPage} from "../logo/LogoPage";
 
 import styles from "./Main.module.scss";
 
 export const Main = () => (
   <PageWrapper>
     <LogoImage className={styles.logoImage} />
-    *Insert fancy text here*
   </PageWrapper>
 );
