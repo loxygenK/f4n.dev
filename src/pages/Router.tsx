f7fcfeab4f011365b59117d30a63533c6c5a1a54
loxygenk <me@loxygen.dev>
2021-10-01 00:14:22 +0900

    ✨ Create LogoPage and register to router

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ContentWrapper } from "~/comps/layout/CotentWrapper";
+import {LogoPage} from "./logo/LogoPage";
 import { Main } from "./main/Main";
 import { Splash } from "./splash/Splash";
 
@@ -10,6 +11,7 @@ export const AppRouter = () => (
       <Switch>
         <Route exact path="/" component={Splash} />
         <Route exact path="/me" component={Main} />
+        <Route exact path="/home" component={ LogoPage } />
       </Switch>
     </BrowserRouter>
   </ContentWrapper>
---
+++ src/pages/logo/LogoPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {PageWrapper} from '~/comps/layout/PageWrapper';
+import {LogoImage} from '~/comps/shared/LogoImage';
+
+export const LogoPage = () => (
+  <PageWrapper>
+    <LogoImage />
+  </PageWrapper>
+)
