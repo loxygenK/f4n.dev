90cb9d2c4da76e56f7762082b72ffda9b4cfae30
loxygenk <me@loxygen.dev>
2021-10-01 00:29:07 +0900

    🎨 Specify the size of SVG image

--- src/pages/logo/LogoPage.tsx
+++ src/pages/logo/LogoPage.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {PageWrapper} from '~/comps/layout/PageWrapper';
 import {LogoImage} from '~/comps/shared/LogoImage';
 
+import styles from "./Logo.module.scss";
+
 export const LogoPage = () => (
   <PageWrapper>
-    <LogoImage />
+    <LogoImage className={styles.logoImage}/>
   </PageWrapper>
 )
