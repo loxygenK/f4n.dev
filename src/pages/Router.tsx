833119ac17bfdffd2eae40cabea52afe3b69d3da
loxygenk <me@loxygen.dev>
2021-10-11 00:41:08 +0900

    ✨ Prepare for implementing animation

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,16 +1,36 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, RouteComponentProps, Switch, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { ContentWrapper } from "~/comps/layout/CotentWrapper";
 import { Main } from "./main/Main";
 import { Splash } from "./splash/Splash";
 
-export const AppRouter = () => (
-  <ContentWrapper>
+class InternalAppRouter extends React.Component<RouteComponentProps> {
+  render() {
+    return (
+      <ContentWrapper>
+        <TransitionGroup>
+          <CSSTransition
+            key={this.props.location.key}
+            classNames="hoge"
+            timeout={200}>
+              <Switch>
+                <Route exact path="/" component={Splash} />
+                <Route exact path="/me" component={Main} />
+              </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      </ContentWrapper>
+    );
+  }
+}
+
+export const AppRouter = () => {
+  const WithRoutered = withRouter(InternalAppRouter);
+
+  return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Splash} />
-        <Route exact path="/me" component={Main} />
-      </Switch>
+      <WithRoutered />
     </BrowserRouter>
-  </ContentWrapper>
-);
+  );
+};
