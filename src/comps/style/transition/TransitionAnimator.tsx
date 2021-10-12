5a5e2650f553780aa0ab425660945afa271cebe3
loxygenk <me@loxygen.dev>
2021-10-12 23:35:47 +0900

    ♻ Swap ContentWrapper and TransitionAnimator

--- src/comps/style/transition/TransitionAnimator.tsx
+++ src/comps/style/transition/TransitionAnimator.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import {CSSTransitionClassNames} from "react-transition-group/CSSTransition";
+import { CSSTransitionClassNames } from "react-transition-group/CSSTransition";
 
 import styles from "./TransitionAnimator.module.scss";
 
 const transitionClassNames: CSSTransitionClassNames = {
-          enter: styles.pageEnter,
-          enterActive: styles.pageEnterActive,
-          exit: styles.pageExit,
-          exitActive: styles.pageExitActive,
-        };
+  enter: styles.pageEnter,
+  enterActive: styles.pageEnterActive,
+  exit: styles.pageExit,
+  exitActive: styles.pageExitActive,
+};
 
 export type TransitionAnimatorProps = {
   children: React.ReactNode;
-  enableAnimation: boolean
+  enableAnimation: boolean;
 };
 
-export const TransitionAnimator = ({ children, enableAnimation }: TransitionAnimatorProps) => {
+export const TransitionAnimator = ({
+  children,
+  enableAnimation,
+}: TransitionAnimatorProps) => {
   const location = useLocation();
 
   console.log(location.pathname);
--- src/pages/router/Router.tsx
+++ src/pages/router/Router.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
 import { ContentWrapper } from "~/comps/layout/CotentWrapper";
-import { TransitionAnimator } from "~/pages/transition/TransitionAnimator";
-import {routingTable} from "./routing";
+import { TransitionAnimator } from "~/comps/style/transition/TransitionAnimator";
+import { routingTable } from "./routing";
 
 export const InternalAppRouter = () => {
   const location = useLocation();
 
-  const enableAnimation = routingTable.find((p) => p.path == location.pathname)?.transactionAnimation ?? true;
+  const enableAnimation =
+    routingTable.find((p) => p.path == location.pathname)
+      ?.transactionAnimation ?? true;
 
   return (
-    <ContentWrapper>
-      <TransitionAnimator enableAnimation={enableAnimation}>
+    <TransitionAnimator enableAnimation={enableAnimation}>
+      <ContentWrapper>
         <Switch location={location}>
-          {
-            routingTable.map((r, i) => <Route key={i} {...r} />)
-          }
+          {routingTable.map((r, i) => (
+            <Route key={i} {...r} />
+          ))}
         </Switch>
-      </TransitionAnimator>
-    </ContentWrapper>
+      </ContentWrapper>
+    </TransitionAnimator>
   );
 };
 
