20a7def0d8be7e5256cdf1064f4eb5554a9b15d4
loxygenK <me@loxygen.dev>
2021-10-13 09:51:03 +0900

    ✨ Create new about page

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {PageWrapper} from "~/comps/layout/PageWrapper";
+import {Heading} from "~/comps/shared/Heading";
+
+export const About = () => (
+  <PageWrapper>
+    <Heading sub="About">自己紹介</Heading>
+    <div>Hi! This is the about page.</div>
+  </PageWrapper>
+);
