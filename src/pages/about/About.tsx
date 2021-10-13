6cae4d83aba40fd8e79179f9d8436d093a26983c
loxygenK <me@loxygen.dev>
2021-10-13 11:20:55 +0900

    ✨ Create avatar component

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import {PageWrapper} from "~/comps/layout/PageWrapper";
 import {Heading} from "~/comps/shared/Heading";
 
+import styles from "./About.module.scss";
+import {Avatar} from "./Avatar";
+import {Introduction} from "./Introduction";
+
 export const About = () => (
   <PageWrapper>
-    <Heading sub="About">自己紹介</Heading>
-    <div>Hi! This is the about page.</div>
+    <Heading sub="About" emoji="🙌">自己紹介</Heading>
+    <div className={styles.container}>
+      <Avatar />
+      <Introduction />
+    </div>
   </PageWrapper>
 );
---
+++ src/pages/about/Introduction.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Introduction.module.scss";
+
+export const Introduction = () => (
+  <div>
+    フライさんです．
+  </div>
+);
