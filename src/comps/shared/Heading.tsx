8878d3b5a99e036d3303f1ff20622f8b13e32c16
loxygenK <me@loxygen.dev>
2021-10-13 10:19:58 +0900

    ✨ Create component to show emoji in Twemoji

---
+++ src/comps/shared/Heading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Twemoji from "react-twemoji";
+import {Emoji} from "./Emoji";
+
+import styles from "./Heading.module.scss";
+
+export type HeadingProps = {
+  children: string;
+  emoji: string;
+  sub: string;
+};
+export const Heading = ({children, emoji, sub}: HeadingProps) => (
+  <h1 className={styles.heading}>
+    <Emoji emoji={emoji} />
+    <span>{children}</span>
+    {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+    <span className={styles.subText}>// {sub}</span>
+  </h1>
+)
