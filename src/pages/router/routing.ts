a4de89b01f574577fe53b26695205457b67398ee
loxygenk <me@loxygen.dev>
2021-11-08 22:49:59 +0900

    🍱 Create quick navigation list

--- src/pages/router/routing.ts
+++ src/pages/router/routing.ts
@@ -3,8 +3,19 @@ import { Splash } from "~/pages/splash/Splash";
 import { Main } from "~/pages/main/Main";
 import { About } from "../about/About";
 
+export type QuickNavigation = {
+  path: string,
+  caption: string
+}
+
 export const routingTable: RouteProps[] = [
   { path: "/", component: Splash, exact: true },
   { path: "/me", component: Main, exact: true },
   { path: "/about", component: About, exact: true },
 ];
+
+export const quickNavigations: QuickNavigation[] = [
+  { path: "/about", caption: "自己紹介" },
+  { path: "/skills", caption: "スキル" },
+  { path: "/careers", caption: "実績" },
+]
