4d50147c4b5cf84d13bc1ae9aefc165b538c878c
loxygenk <me@loxygen.dev>
2021-11-20 16:54:44 +0900

    ✨ Create AwaitFetch component

--- src/pages/about/organisms/Introduction.tsx
+++ src/pages/about/organisms/Introduction.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AwaitFetch } from "~/comps/shared/AwaitFetch";
 import { Affiliation } from "../atoms/Affiriation";
 import { useBasicAPI } from "../controller";
 
@@ -7,21 +8,24 @@ import styles from "./Introduction.module.scss";
 export const Introduction = () => {
   const { loading, error, data } = useBasicAPI();
 
-  if (loading || data === undefined) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.name}>
-        <span className={styles.primary}>{data.basic.name.primary}</span>
-        <span className={styles.aka}>{data.basic.name.aka.join(" / ")}</span>
-      </div>
-      <div className={styles.affiriation}>
-        {data.basic.affiliation.map((a, key) => (
-          <Affiliation {...a} key={key} />
-        ))}
-      </div>
-      <p>{data.basic.introduction}</p>
-    </div>
+    <AwaitFetch loading={loading} error={error} data={data}>
+      {(data) => (
+        <div className={styles.wrapper}>
+          <div className={styles.name}>
+            <span className={styles.primary}>{data.basic.name.primary}</span>
+            <span className={styles.aka}>
+              {data.basic.name.aka.join(" / ")}
+            </span>
+          </div>
+          <div className={styles.affiriation}>
+            {data.basic.affiliation.map((a, key) => (
+              <Affiliation {...a} key={key} />
+            ))}
+          </div>
+          <p>{data.basic.introduction}</p>
+        </div>
+      )}
+    </AwaitFetch>
   );
 };
