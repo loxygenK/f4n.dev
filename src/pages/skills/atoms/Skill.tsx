77a48f57ef8faeab437a229a1b5de557e0b56dd1
loxygenk <me@loxygen.dev>
2021-11-21 18:28:56 +0900

    🎨 Style Skill element

--- src/pages/skills/atoms/Skill.tsx
+++ src/pages/skills/atoms/Skill.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { SkilledLevel } from "~/api/graphql/autogen/scheme";
+import { combineClassName } from "~/utils/combineClassName";
+
+import styles from "./Skill.module.scss";
+
+const skilledLevelToCaption: { [key in SkilledLevel]: string } = {
+  FAVORITE: "好んで使う",
+  ADVANCED: "それなりに使える",
+  EXPERIENCED: "経験アリ",
+  BEGINNER: "初心者; 業務経験なし",
+  STUDYING: "勉強中",
+};
 
 export type SkillProps = {
   caption: string;
   level: SkilledLevel;
 };
 export const Skill: React.VFC<SkillProps> = ({ caption, level }) => (
-  <div>
-    <span>{caption}</span>
-    <span>{level}</span>
+  <div className={styles.container}>
+    <span className={styles.name}>{caption}</span>
+    <span className={styles.level}>{skilledLevelToCaption[level]}</span>
   </div>
 );
