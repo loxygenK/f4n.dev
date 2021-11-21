87e7209a1c75b5bbbef6db05f113a92654669bec
loxygenk <me@loxygen.dev>
2021-11-21 21:19:10 +0900

    ✨ Create the stab of Work page + register to the router

---
+++ src/pages/works/Works.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { PageWrapper } from "~/comps/layout/PageWrapper";
+import { Heading } from "~/comps/shared/Heading";
+
+export const Works = () => (
+  <PageWrapper>
+    <Heading sub="works" emoji="✨">
+      作ったもの
+    </Heading>
+  </PageWrapper>
+);
