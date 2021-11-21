05289bd2dda0186e6ed6a65f546a1d784998ae00
loxygenk <me@loxygen.dev>
2021-11-21 13:16:06 +0900

    ✨ Retrieve skill information from API

--- src/pages/skills/Skills.tsx
+++ src/pages/skills/Skills.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { SkillType } from "~/api/graphql/autogen/scheme";
 import { PageWrapper } from "~/comps/layout/PageWrapper";
 import { Heading } from "~/comps/shared/Heading";
+import { SkillList, SkillListProps } from "./molecules/SkillList";
+
+import styles from "./Skills.module.scss";
+
+const skillListTable: Map<
+  SkillType,
+  Omit<SkillListProps, "skillType">
+> = new Map([
+  ["FRONTEND", { emoji: "🎨", caption: "フロントエンド" }],
+  ["BACKEND", { emoji: "💿", caption: "バックエンド" }],
+  ["INFRASTRUCTURE", { emoji: "🏗️", caption: "インフラストラクチャー" }],
+  ["UTILITY", { emoji: "🔨", caption: "その他" }],
+]);
 
 export const Skills = () => (
   <PageWrapper>
     <Heading sub="skills" emoji="📝">
       スキル
     </Heading>
-    <span>Hello!</span>
+    <div className={styles.container}>
+      {Array.from(skillListTable.entries()).map(([k, v], i) => (
+        <SkillList {...v} skillType={k} key={i} />
+      ))}
+    </div>
   </PageWrapper>
 );
---
+++ src/pages/skills/controller.ts
@@ -0,0 +1,30 @@
+import { gql, QueryResult, useQuery } from "@apollo/client";
+import { Skill, SkillType } from "~/api/graphql/autogen/scheme";
+
+const FetchSkills = gql`
+  query FetchSkill {
+    skills {
+      name
+      skillType
+      level
+    }
+  }
+`;
+type FetchSkillsResponse = { skills: Skill[] };
+export const useSkillsAPI = (): QueryResult<FetchSkillsResponse> =>
+  useQuery<FetchSkillsResponse>(FetchSkills);
+
+export const useSkillsTypeFilteredAPI = (
+  type: SkillType
+): QueryResult<FetchSkillsResponse> => {
+  const skillAPIReturn = useSkillsAPI();
+
+  if (skillAPIReturn.data === undefined) return skillAPIReturn;
+
+  return {
+    ...skillAPIReturn,
+    data: {
+      skills: skillAPIReturn.data.skills.filter((s) => s.skillType === type),
+    },
+  };
+};
