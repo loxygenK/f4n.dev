339064a6eb94e109d358591e4d591b020a000f00
loxygenk <me@loxygen.dev>
2021-11-21 20:15:50 +0900

    ✨ Create the stab of career page

---
+++ src/pages/careers/Careers.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { PageWrapper } from "~/comps/layout/PageWrapper";
+import { Heading } from "~/comps/shared/Heading";
+
+export const Careers = () => (
+  <PageWrapper>
+    <Heading sub="careers" emoji="🧑‍💼">
+      実績
+    </Heading>
+  </PageWrapper>
+);
